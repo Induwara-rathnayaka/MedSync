1c8a510802b14496855476026291c792a6584945
Induwar-Rathnaayaka <rathnay-ps20245@stu.kln.ac.lk>
2025-03-14 08:03:36 +0530

    name change booking delete

--- src/pages/Patient/BookingList.tsx
+++ src/pages/Patient/BookingList.tsx
@@ -66,7 +66,7 @@ export const BookingList: React.FC = () => {
                                     onClick={() => handleDelete(book.id, book.doctorName, book.day, book.time)}
                                     className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
                                 >
-                                    Delete
+                                    Cancel Booking
                                 </button>
                             </div>
                         </div>
